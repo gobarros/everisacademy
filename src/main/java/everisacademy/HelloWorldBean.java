4a1bce1fb1cd21100e1b5aaf712220f1fa377e98
Ana Roque <anaroque86@gmail.com>
2020-01-25 13:09:55 +0000

    First commit

---
+++ src/main/java/everisacademy/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package everisacademy;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean(name = "helloworld")
+@ViewScoped
+public class HelloWorldBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8425256913664822653L;
+
+	private String message = "mechelas";
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
